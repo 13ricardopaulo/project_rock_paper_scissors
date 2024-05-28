2d5fc18f61116443a643d4b45542fed5fa74bb54
13ricardopaulo <169499968+13ricardopaulo@users.noreply.github.com>
2024-05-28 18:12:25 +0100

    Create playRound function
    Write the logic to play a single game.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//COMPUTER CHOICE
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
 
@@ -10,10 +11,35 @@ function getComputerChoice() {
   }
   return randomNumber;
 }
-
+//PLAYER CHOICE
 function getHumanChoice() {
   let playerInput = prompt("Please enter Rock, Paper or, Scissors!");
   return playerInput;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    humanScore = ++humanScore;
+    console.log("You Win!");
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    humanScore = ++humanScore;
+    console.log("You Win!");
+  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    humanScore = ++humanScore;
+    console.log("You Win!");
+  } else {
+    computerScore = ++computerScore;
+    console.log("Computer Wins!");
+  }
+
+  console.log("Your score:" + "", humanScore);
+  console.log("Computer score:" + "", computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
