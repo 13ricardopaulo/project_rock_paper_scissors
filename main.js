0d151737356432721974448e4d015c8bc8af2fc4
13ricardopaulo <169499968+13ricardopaulo@users.noreply.github.com>
2024-06-04 20:03:58 +0100

    New Solution
    Start from scratch

--- main.js
+++ main.js
@@ -1,51 +1,68 @@
-//COMPUTER CHOICE
+//GET RANDOM NUMBER TO CHOOSE ONE OF THE OPTIONS TO BE THE COMPUTER CHOICE
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
 
-  if (randomNumber === 0) {
+  if (randomNumber == 0) {
     randomNumber = "rock";
-  } else if (randomNumber === 1) {
+  } else if (randomNumber == 1) {
     randomNumber = "paper";
   } else {
     randomNumber = "scissors";
   }
   return randomNumber;
 }
-//PLAYER CHOICE
+
+//GET HUMAN CHOICE AND MAKE IT CASE INSENSITIVE
 function getHumanChoice() {
-  let playerInput = prompt("Please enter Rock, Paper or, Scissors!");
-  return playerInput;
+  let humanChoice = prompt(
+    "Please enter Rock, Paper or Scissors!"
+  ).toLowerCase();
+  return humanChoice;
 }
 
+//GLOBAL SCOPE VARIABLES
 let humanScore = 0;
 let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
-    humanScore = ++humanScore;
-    console.log("You Win!");
-  } else if (
-    humanChoice.toLowerCase() === "paper" &&
-    computerChoice === "rock"
-  ) {
-    humanScore = ++humanScore;
-    console.log("You Win!");
-  } else if (
-    humanChoice.toLowerCase() === "scissors" &&
-    computerChoice === "paper"
-  ) {
-    humanScore = ++humanScore;
-    console.log("You Win!");
-  } else {
-    computerScore = ++computerScore;
-    console.log("Computer Wins!");
-  }
-
-  console.log("Your score:" + "", humanScore);
-  console.log("Computer score:" + "", computerScore);
-}
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+//LOGIC FOR A SINGLE ROUND
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log(
+      `You chose: ${humanSelection} --- Computer chose: ${computerSelection}`
+    );
+    console.log("TIE GAME!!");
+    console.log(`You: ${humanScore} --- Computer: ${computerScore}`);
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log(
+      `You chose: ${humanSelection} --- Computer chose: ${computerSelection}`
+    );
+    console.log(`YOU WIN! ${humanSelection} beats ${computerSelection}!`);
+    humanScore = humanScore + 1;
+    console.log(`You: ${humanScore} --- Computer: ${computerScore}`);
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log(
+      `You chose: ${humanSelection} --- Computer chose: ${computerSelection}`
+    );
+    console.log(`YOU WIN! ${humanSelection} beats ${computerSelection}!`);
+    humanScore = humanScore + 1;
+    console.log(`You: ${humanScore} --- Computer: ${computerScore}`);
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log(
+      `You chose: ${humanSelection} --- Computer chose: ${computerSelection}`
+    );
+    console.log(`YOU WIN! ${humanSelection} beats ${computerSelection}!`);
+    humanScore = humanScore + 1;
+    console.log(`You: ${humanScore} --- Computer: ${computerScore}`);
+  } else {
+    console.log(
+      `You chose: ${humanSelection} --- Computer chose: ${computerSelection}`
+    );
+    console.log(`YOU LOSE! ${computerSelection} beats ${humanSelection}`);
+    computerScore = computerScore + 1;
+    console.log(`You: ${humanScore} --- Computer: ${computerScore}`);
+  }
+}
 
 playRound(humanSelection, computerSelection);
